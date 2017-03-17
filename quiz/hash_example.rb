208e195ee301a1ec0c3d305483f7a7a136b0471a
malexander15 <creepinjezus15>
2017-03-16 23:49:14 -0400

    added a quiz question on hashes as good reference example

---
+++ quiz/hash_example.rb
@@ -0,0 +1,4 @@
+# Create a hash for a snowy_owl, a bird that is a carnivore and has a life span of 10 years.
+
+snowy_owl = { "type" => "bird", "diet" => "carnivore", "life_span" => "10 years"}
+puts snowy_owl["type"]
