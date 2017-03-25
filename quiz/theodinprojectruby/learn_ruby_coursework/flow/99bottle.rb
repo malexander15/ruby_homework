8d2dc02c920bc919f0b8d7ac15b7aebc3915d53e
malexander15 <creepinjezus15>
2017-03-25 14:05:43 -0400

    made file in flow for deaf grandma program

---
+++ quiz/theodinprojectruby/learn_ruby_coursework/flow/99bottle.rb
@@ -0,0 +1,5 @@
+count = 99
+while count =! 1
+puts count.to_s + " bottles of beer on the wall " + count.to_s + " bottles of beer. Take one down pass it around " + count.to_s + " bottles of beer on the wall."
+count -= 1
+end
